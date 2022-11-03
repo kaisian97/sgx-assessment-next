48cc20c532d05876df565b66005d9bf37b25a773
kaisian97 <kaisian97@hotmail.com>
2022-11-04 00:08:47 +0800

    add constant

---
+++ constant/index.ts
@@ -0,0 +1,27 @@
+export const SIZE = {
+  LG: 1280,
+  MD: 1024,
+  SM: 768,
+  XS: 640,
+}
+
+export const IMAGE_MIME_TYPES = [
+  '.jpeg',
+  '.jpg',
+  '.gif',
+  '.png',
+  '.avif',
+  '.bmp',
+  '.tiff',
+  '.webp',
+]
+
+export const VIDEO_MIME_TYPES = [
+  '.mp4',
+  '.mpeg',
+  '.ogg',
+  '.mp2t',
+  '.webm',
+  '.3gpp',
+  '.3gpp2',
+]
